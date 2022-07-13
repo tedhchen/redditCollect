13e45a31ae450bb900d46eb0dc7d4f2f6daf4a03
juansrico <107045777+juansrico@users.noreply.github.com>
2022-07-13 10:11:08 +0300

    Update and rename Scheme Pushshift data extraction to Scheme Pushshift data extraction.py

--- Scheme Pushshift data extraction.py
+++ Scheme Pushshift data extraction.py
@@ -1,4 +1,5 @@
 ## Main steps to extract reddit data via Pushshift ##
+"""
 Logic of the code:
 1. download all reddit data 
 2. verify that the desired data is the same as the one extracted using filters (parameters)
@@ -10,3 +11,9 @@ Next steps:
 - Figure out how to download all Reddit data
 - Explore whether the data formats for datasets exctracted using PRAW are similar/same to those using Pushshift (e.g. same columns?) 
 - Sketch the main functions 
+"""
+
+pip install pmaw pandas
+import pandas as pd
+from pmaw import PushshiftAPIapi = PushshiftAPI()
+
