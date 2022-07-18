bb9cacdb383e0dba5ef7b9f1278c4ce15fd401e7
Sonja Savolainen <70197274+sonjasav@users.noreply.github.com>
2022-07-18 14:32:45 +0300

    Update PushshiftAPI.py

--- PushshiftAPI.py
+++ PushshiftAPI.py
@@ -1,12 +1,13 @@
 import psaw
 from psaw import PushshiftAPI
+import pandas as pd
 
 api = PushshiftAPI()
 
 posts = list(api.search_submissions(
         subreddit='stamps', limit = 10))
 
-df = pd.DataFrame(posts)
+df = pd.DataFrame([thing.d_ for thing in posts])
 
 print(df)
 
