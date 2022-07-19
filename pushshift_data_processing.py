c0297f9382d30111774975c3684e1b9dc8da119f
Sonja Savolainen <70197274+sonjasav@users.noreply.github.com>
2022-07-19 16:02:25 +0300

    Update pushshift_data_processing.py

--- pushshift_data_processing.py
+++ pushshift_data_processing.py
@@ -1,3 +1,6 @@
+#Note: for this code, we need to decompress and run the code for every monthly file separately
+    #Is there a better way to decompress and run selected monthly files at once?
+
 import json
 import pandas as pd
 
@@ -17,9 +20,38 @@ submissions = [value for value in submissions if value['key'] in valuelist] #fil
 #can be used to retrieve submissions where title or selftext includes searchwords
 submissions = [value for value in submissions if 'searchword1' in value['key'].lower() or 'searchword2' in value['key'].lower() and 'searchword3' in value['key'].lower()] #filter items
 
-#Format the filtered list as a dataframe
-df = pd.DataFrame(submissions)
-df.head()
+#Format the filtered list of submissions as a dataframe
+df_rs = pd.DataFrame(submissions)
+df_rs.head()
+
+#Filter comments
+
+#Open comment file
+comments = []
+for line in open('filename.json'):
+    comments.append(json.loads(line))
+    
+#Use list comprehension to retrieve comments, where link_id is equal to any id of selected submissions
+link_id_list = df['id'].tolist() #define set of link_ids
+comments = [value for value in comments if value['link_id'] in link_id_list] #filter items
+
+#Format the filtered list of comments as a dataframe
+df_rc = pd.DataFrame(comments)
+df_rc.head()
+
+#Saving comments and submissions separately
+df_rs.to_csv('RS_set_year_month.csv')
+df_rc.to_csv('RC_set_year_month.csv')
+
+#Combining comments and submissions
+df_rs['type'] = 'submission'   #Add a type column to indicate item type (comment or submission)
+df_rc['type'] = 'comment'     #Add a type column to indicate item type (comment or submission)
+
+df_rs.rename(columns = {'id':'link_id'}, inplace = True)   #Unify headers of overlapping id column
+
+reddit_data = pd.concat([df_rs, df_rc])   #Concenate comments and submissions
+
+reddit_data.to_csv('REDDIT_set_year_month.csv')
 
 #Note: post_hint column seems to have 'image' as value if the post contains a photo
 #Note: The column link_id would allow us to match comments with submission. Link: https://galenweld.github.io/reddit_join_comments_example/
