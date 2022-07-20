9a072906e2c6b4c694a0be486b2159342819486f
juansrico <107045777+juansrico@users.noreply.github.com>
2022-07-20 12:58:44 +0300

    Update Datetime to unix.py

--- Datetime to unix.py
+++ Datetime to unix.py
@@ -9,9 +9,9 @@ import pytz
 tz = pytz.timezone("UTC")
 
 #1- assign the desired date to a variable
-date_time_after = datetime.datetime(2021, 7, 26, 21, 20) # year, month, day, hour, minute
-date_time_after = tz.localize(date_time_after) # Localizing the time zone to Helsinki 
- 
+
+date_time_after = tz.localize(datetime.datetime(2021, 7, 26, 21, 20)) # Year, month, day, hour, minute | # tz.localize is to set the UTC time zone  
+
 # optional: print regular python date&time
 print("date_time =>",date_time_after)
  
@@ -24,8 +24,7 @@ print("unix_timestamp => ",unix_time_after)
      
   
 ### Repeat the process for the date "before" | Short version: ###
-date_time_before = datetime.datetime(2022, 6, 26, 21, 20) 
-date_time_before = tz.localize(date_time_before) 
+date_time_before = tz.localize(datetime.datetime(2022, 7, 26, 21, 20)) 
 unix_time_before = time.mktime(date_time_before.timetuple())
 unix_time_before = int(unix_time_before)
   
