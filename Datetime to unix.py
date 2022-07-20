23628d9930e552703d0a919e7ed515795af2a4d7
juansrico <107045777+juansrico@users.noreply.github.com>
2022-07-20 11:12:35 +0300

    Create Datetime to unix.py

---
+++ Datetime to unix.py
@@ -0,0 +1,31 @@
+import datetime
+import time
+import pytz
+
+
+### Add the following code to the filter of submissions/comments ###
+
+#0- # defining the timezone to Helsinki
+tz = pytz.timezone("Europe/Helsinki")
+
+#1- assign the desired date to a variable
+date_time_after = datetime.datetime(2021, 7, 26, 21, 20) # year, month, day, hour, minute
+date_time_after = tz.localize(date_time_after) # Localizing the time zone to Helsinki 
+ 
+# optional: print regular python date&time
+print("date_time =>",date_time_after)
+ 
+#2- convert datetime to unix and assign it to a variable
+unix_time_after = time.mktime(date_time_after.timetuple())
+  
+# optional: display unix timestamp after conversion
+print("unix_timestamp => ",unix_time_after)
+     
+  
+### Repeat the process for the date "before" | Short version: ###
+date_time_before = datetime.datetime(2022, 6, 26, 21, 20) 
+date_time_before = tz.localize(date_time_before) 
+unix_time_before = time.mktime(date_time_before.timetuple())
+  
+  
+#3- Include both variables as parameters in the filter 
